3489de88e8dbf4bbc43a438c54fcc7ef638d75f2
Mike Hopper <drmikeh@gmail.com>
2016-03-16 09:06:09 -0400

    Fixed bug with admin view changing a user's cohort.

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -13,51 +13,37 @@ class AdminController {
   }
 
   // TODO: handle newly created cohorts
-  loadCohorts() {
-    return this.cohorts.length ? null : this.$http.get('/api/cohorts')
-    .then((response) => {
-      console.log('we have cohorts');
-      this.cohorts = response.data;
-    });
-  }
-
-  setCohortHelper(user) {
-    console.log('setCohortHelper: finding cohort with _id:', user.cohort._id);
-    console.log('in this.cohorts:', this.cohorts);
-    var selected = this.$filter('filter')(this.cohorts,
-                                          {_id: user.cohort._id}
-                                         );
-    user.cohort = selected.length ? selected[0] : null;
-    console.log('setCohortHelper:', user.cohort);
-  }
-
-  setCohort(user, cohort) {
-    if (user.cohort) {
-      if (this.cohorts.length === 0) {
-        this.loadCohorts().then(() => {
-          console.log('this.cohorts:', this.cohorts);
-          this.setCohortHelper(user);
-        });
-      }
-      else {
-        setCohortHelper(user);
-      }
+  loadCohorts(user) {
+    if (this.cohorts.length) {
+      this.setCohortInVM(user, user.cohort);
+      return null;
+    }
+    else {
+      return this.$http.get('/api/cohorts')
+      .then((response) => {
+        this.cohorts = response.data;
+        this.setCohortInVM(user, user.cohort);
+      });
     }
   }
 
+  setCohortInVM(user, cohort) {
+    var selected = this.$filter('filter')(this.cohorts,
+                                          {_id: cohort._id}
+                                         );
+    user.cohort = selected.length ? selected[0] : null;
+  }
+
   updateRole(user, role) {
-    console.log('updateRole:', user, role);
     return this.$http.put('/api/users/' + user._id + '/role', { role: role } );
   }
 
   updateCohort(user, cohort) {
-    console.log('updateCohort:', user, cohort._id);
     return this.$http.put('/api/users/' + user._id + '/cohort',
                           { cohort: cohort._id }
                          )
     .then((response) => {
-      console.log('handling response');
-      this.setCohortHelper(user);
+      this.setCohortInVM(user, cohort);
     });
   }
 
