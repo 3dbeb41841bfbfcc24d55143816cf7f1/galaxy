eb71f9b06ca1629f22b6a317e11123d86ecb4c9b
Dr. Mike Hopper <drmikeh@gmail.com>
2016-03-23 17:10:46 -0400

    Added auto-update for github authenticated users.

--- server/auth/github/passport.js
+++ server/auth/github/passport.js
@@ -12,7 +12,7 @@ exports.setup = function(User, config) {
     User.findOne({
       'github.id': Number(profile.id)
     })
-    .then(function(user) {
+    .then(user => {
       if (!user) {
         return Cohort.findOne({ name: 'ATL WDI #6' })
         .then(currentCohort => {
@@ -25,19 +25,29 @@ exports.setup = function(User, config) {
             github: profile._json
           });
           user.save()
-          .then(function(user) {
+          .then(user => {
             return done(null, user);
           })
-          .catch(function(err) {
+          .catch(err => {
             return done(err);
           });
         });
       }
       else {
-        return done(null, user);
+        // TODO: update user
+        user.name = profile.displayName;
+        user.username = profile.username;
+        user.github = profile._json;
+        user.save()
+        .then(user => {
+          return done(null, user);
+        })
+        .catch(err => {
+          return done(err);
+        });
       }
     })
-    .catch(function(err) {
+    .catch(err => {
       return done(err);
     });
   }));
