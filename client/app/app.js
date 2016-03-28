dfe9c91278a67cec4cb02cfcae26ef709843ee5b
Dr. Mike Hopper <drmikeh@gmail.com>
2016-03-28 13:03:32 -0400

    Added animation for inner views and ngBusy spinning icon when waiting for AJAX responses.

--- client/app/app.js
+++ client/app/app.js
@@ -7,18 +7,20 @@ angular.module('galaxyApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
+  'ngAnimate',
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
   'validation.match',
-  'xeditable'
+  'xeditable',
+  'ngBusy'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
+.config(function($urlRouterProvider, $locationProvider) {
+  $urlRouterProvider
+    .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  })
-  .run(function(editableOptions) {
-    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-  });
+  $locationProvider.html5Mode(true);
+})
+.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
