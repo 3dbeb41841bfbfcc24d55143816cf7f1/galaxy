af59907303fd62f9ac6b7d7f7049c5b2e131c132
Dr. Mike Hopper <drmikeh@gmail.com>
2016-08-05 18:43:12 -0400

    Added some content to the Student Dashboard.

--- client/app/student/dashboard/student-dashboard-controller.js
+++ client/app/student/dashboard/student-dashboard-controller.js
@@ -7,6 +7,23 @@
       console.log('StudentDashboardController is alive!');
       this.getCurrentUser = Auth.getCurrentUser;
     }
+
+    // TODO: start of copy paste from student-profiles-controller.js
+    getAttendancePresentOrLate(user) {
+      var presentOrLate = 0;
+      user.attendance.forEach((a) => {
+        if (a.value === 'present' || a.value === 'late') {
+          ++presentOrLate;
+        }
+      });
+      return presentOrLate;
+    }
+
+    getAttendancePercentage(user) {
+      var total = user.attendance.length;
+      return total === 0 ? 0.0 : this.getAttendancePresentOrLate(user) * 100.0 / total;
+    }
+    // TODO: end of copy paste from student-profiles-controller.js
   }
 
   angular.module('galaxyApp')
